c97c4d630bf4441a65e3c30280bd08910718ed29
adityawintech <aditya#19AI>
2025-04-19 12:21:40 +0530

    Aditya

---
+++ src/Components/About/AboutHero/index.jsx
@@ -0,0 +1,9 @@
+
+const AboutHero=()=>{
+    return (
+        <>
+        <h1 className="bg-red-500">About Hero</h1>
+        </>
+    )
+}
+export default AboutHero
---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import AboutHero from "@/Components/About/AboutHero"
+
+const About=()=>{
+    return (
+        <>
+        <h1>About</h1>
+        <AboutHero/>
+        </>
+    )
+}
+export default About
